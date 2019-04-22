1d2aa8f57426ad94676d0292115fb583ed19ef54
sascha <sascha@sascha-VirtualBox>
2019-04-23 01:48:10 +0200

    Working 2D Gauss Beam implemented

--- include/CRT_Base_IF.h
+++ include/CRT_Base_IF.h
@@ -588,7 +588,7 @@ void CRT_Base_IF<T,dim,no_int_states>::Numerical_Raman()
     const double dt = -m_header.dt;
     const double t1 = this->Get_t();
 
-    std::array<double,2> laser_k_tmp, laser_w_tmp, z_R, waist_0, z_0, GaussAmp, R, waist, gouy;
+    std::array<double,2> laser_k_tmp, laser_w_tmp, z_R, waist_0, z_0, GaussAmp, R_c, Phase_c, waist, gouy;
 
     vector<fftw_complex *> Psi;
     for ( int i=0; i<no_int_states; i++ )
@@ -617,8 +617,8 @@ void CRT_Base_IF<T,dim,no_int_states>::Numerical_Raman()
     // Gauss Beam Parameter
     z_0[0] = 0;
     z_0[1] = 0;
-    waist_0[0] = 2;
-    waist_0[1] = 2;
+    waist_0[0] = 7; // std::numeric_limits<float>::max() for plane waves
+    waist_0[1] = 7;
     z_R[0] = laser_k_tmp[0]/2 * pow(waist_0[0],2);
     z_R[1] = laser_k_tmp[1]/2 * pow(waist_0[1],2);
     // -------------------
@@ -650,11 +650,22 @@ void CRT_Base_IF<T,dim,no_int_states>::Numerical_Raman()
         gsl_matrix_complex_set(A,i,i, {phi[i],0});
       }
 
-      // 2D Gauss Amplitude
+      // 2D Gauss Amplitude x[0] is z-axis and x[1] is y-axis also called radius
       waist[0] = waist_0[0] * sqrt( 1 + pow((x[0]-z_0[0])/z_R[0], 2) );
       waist[1] = waist_0[1] * sqrt( 1 + pow((x[0]-z_0[0])/z_R[1], 2) );
-      R[0] = x[0] * ( 1 + pow(z_R[0]/(x[0]-z_0[0]),2) );
-      R[1] = x[0] * ( 1 + pow(z_R[1]/(x[0]-z_0[0]),2) );
+      R_c[0] = (x[0]-z_0[0]) * ( 1 + pow(z_R[0]/(x[0]-z_0[0]),2) );
+      R_c[1] = (x[0]-z_0[1]) * ( 1 + pow(z_R[1]/(x[0]-z_0[1]),2) );
+      // The Radius of curvature can be infinite. This is catched here
+      if (R_c[0] != R_c[0]) {
+    	  R_c[0] = std::numeric_limits<float>::max();
+      }
+      if (R_c[1] != R_c[1]) {
+    	  R_c[1] = std::numeric_limits<float>::max();
+      }
+      //-----------------------------------
+      Phase_c[0] = pow(x[1],2)/(2*R_c[0]);
+      Phase_c[1] = pow(x[1],2)/(2*R_c[1]);
+
       gouy[0] = atan( (x[0]-z_0[0])/z_R[0] );
       gouy[1] = atan( (x[0]-z_0[0])/z_R[1] );
 
@@ -666,8 +677,8 @@ void CRT_Base_IF<T,dim,no_int_states>::Numerical_Raman()
       //Raman
       //---------------------------------------------
 
-      sincos(laser_k_tmp[0] * (x[0] + pow(x[1],2)/(2*R[0]) ) * (doppler_beta - 1) - doppler_beta * laser_w_tmp[0] * t1 - Phi_1_sm[0], &im1, &re1 ); // For right going light
-      sincos(laser_k_tmp[0] * (x[0] + pow(x[1],2)/(2*R[0]) ) * (doppler_beta + 1) + doppler_beta * laser_w_tmp[0] * t1 - Phi_1_sm[1], &im2, &re2 ); // For left going light
+      sincos(laser_k_tmp[0] * (x[0] + Phase_c[0] ) * (doppler_beta - 1) - doppler_beta * laser_w_tmp[0] * t1 - Phi_1_sm[0] - gouy[0], &im1, &re1 ); // For right going light
+      sincos(laser_k_tmp[0] * (x[0] + Phase_c[0] ) * (doppler_beta + 1) + doppler_beta * laser_w_tmp[0] * t1 - Phi_1_sm[1] - gouy[0], &im2, &re2 ); // For left going light
 
       //---------------------------------------------
 
@@ -681,8 +692,8 @@ void CRT_Base_IF<T,dim,no_int_states>::Numerical_Raman()
 
       //---------------------------------------------
 
-      sincos(laser_k_tmp[1] * (x[0] + pow(x[1],2)/(2*R[1]) ) * (doppler_beta - 1) - doppler_beta * laser_w_tmp[1] * t1 - Phi_2_sm[0], &im1, &re1 );
-      sincos(laser_k_tmp[1] * (x[0] + pow(x[1],2)/(2*R[1]) ) * (doppler_beta + 1) + doppler_beta * laser_w_tmp[1] * t1 - Phi_2_sm[1], &im2, &re2 );
+      sincos(laser_k_tmp[1] * (x[0] + Phase_c[1] ) * (doppler_beta - 1) - doppler_beta * laser_w_tmp[1] * t1 - Phi_2_sm[0] - gouy[1], &im1, &re1 );
+      sincos(laser_k_tmp[1] * (x[0] + Phase_c[1] ) * (doppler_beta + 1) + doppler_beta * laser_w_tmp[1] * t1 - Phi_2_sm[1] - gouy[1], &im2, &re2 );
 
       //---------------------------------------------
 
